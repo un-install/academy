47787ac22afc0e01a604a912b577dea6a8025eef
un-install <elllipsse@gmail.com>
2018-12-20 23:47:01 +0200

    lesson1

---
+++ src/main/java/Temp.java
@@ -0,0 +1,6 @@
+public class Temp {
+    public static void main(String[] args) {
+        int[] i;
+         i = new int[Integer.MAX_VALUE - 8];
+    }
+}
---
+++ src/main/java/Tryy.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Tryy {
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner("")){
+
+        }
+    }
+}
