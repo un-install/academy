a018877d782f981ca9dedb16d69b4938a3d5780d
root <elllipsse@gmail.com>
2019-02-02 04:06:02 +0200

    hw16

--- src/main/java/lesson16/rest/MateGroupServiceClient.java
+++ src/main/java/lesson16/rest/MateGroupServiceClient.java
@@ -24,10 +24,25 @@ public class MateGroupServiceClient {
 			conn = addStudent("18122018/students");
 			showInfo(conn);
 
-			conn = deleteStudent("18122018/students/Stefan/");
+		//	conn = deleteStudent("18122018/students/Stefan/");
+		//	showInfo(conn);
+
+			conn = updateStudent("18122018/students/update/Stefan?name=Abraham");
 			showInfo(conn);
 
-			conn = updateStudent("18122018/students/Stefan?name=Abraham");
+			conn = getHrs("18122018/hrs/");
+			showInfo(conn);
+
+			conn = getHr("18122018/hrs/1");
+			showInfo(conn);
+
+			conn = addHr("18122018/hrs/add");
+			showInfo(conn);
+
+			conn = deleteHr("18122018/hrs/delete/1");
+			showInfo(conn);
+
+			conn = updateStartWorkYear("18122018/hrs/update/start-work-year/4?start-work-year=2000");
 			showInfo(conn);
 
 		} catch (IOException e) {
@@ -86,6 +101,52 @@ public class MateGroupServiceClient {
 	}
 
 	private static HttpURLConnection updateStudent(String pathParam) throws  MalformedURLException, IOException, ProtocolException {
+		URL url = new URL("http://localhost:9998/rs/mate/" + pathParam);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("PUT");
+		conn.setRequestProperty("Accept", "application/json");
+		return conn;
+	}
+
+	private static HttpURLConnection getHrs(String pathParam) throws IOException {
+		URL url = new URL("http://localhost:9998/rs/mate/" + pathParam);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("Accept", "application/json");
+		return conn;
+	}
+	private static HttpURLConnection getHr(String pathParam) throws IOException {
+		URL url = new URL("http://localhost:9998/rs/mate/" + pathParam);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("Accept", "application/json");
+		return conn;
+	}
+
+	private static HttpURLConnection addHr(String pathParam)
+			throws MalformedURLException, IOException, ProtocolException {
+		URL url = new URL("http://localhost:9998/rs/mate/" + pathParam);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setDoOutput(true);
+		conn.setRequestMethod("PUT");
+		conn.setRequestProperty("Content-Type", "application/json");
+		String input = "{\"name\": \"Eric\",\"surname\": \"Clapton\",\"yearOfBorn\": 1997, \"personId\": 4, \"startWorkYear\": 2007	}";
+
+		OutputStream os = conn.getOutputStream();
+		os.write(input.getBytes());
+		os.flush();
+		return conn;
+	}
+
+	private static HttpURLConnection deleteHr(String pathParam) throws  MalformedURLException, IOException, ProtocolException {
+		URL url = new URL("http://localhost:9998/rs/mate/" + pathParam);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("DELETE");
+		conn.setRequestProperty("Accept", "application/json");
+		return conn;
+	}
+
+	private static HttpURLConnection updateStartWorkYear(String pathParam) throws  MalformedURLException, IOException, ProtocolException {
 		URL url = new URL("http://localhost:9998/rs/mate/" + pathParam);
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 		conn.setRequestMethod("PUT");
