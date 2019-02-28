5dbca482612b924897aa3f2d8f718784f3956591
un-install <elllipsse@gmail.com>
2019-02-28 12:44:55 +0200

    output improvements + screenshot

--- src/main/java/homeworkx/demo.java
+++ src/main/java/homeworkx/demo.java
@@ -16,7 +16,29 @@ public class demo {
         EntityDAOImpl<Office> ofdao = new EntityDAOImpl<>(Office.class);
         EntityDAOImpl<Customer> cdao = new EntityDAOImpl<>(Customer.class);
 
-        odao.findEntityById(new BigDecimal(113048)).getCust().getOrders().forEach(f -> System.out.println(f));
+        Order o = odao.findEntityById(new BigDecimal(113048));
+        System.out.println(o);
+
+        System.out.println(o.getProduct());
+        o.getProduct().getOrders().forEach(System.out::println);
+
+        System.out.println(o.getCust());
+        o.getCust().getOrders().forEach(System.out::println);
+
+        System.out.println(o.getCust().getCustRep());
+        o.getCust().getCustRep().getCustomers().forEach(System.out::println);
+        o.getCust().getCustRep().getManagedOffices().forEach(System.out::println);
+        o.getCust().getCustRep().getManagedEmployes().forEach(System.out::println);
+        o.getCust().getCustRep().getOrders().forEach(System.out::println);
+        System.out.println(o.getCust().getCustRep().getManager());
+
+
+        System.out.println(o.getCust().getCustRep().getRepOffice());
+        o.getCust().getCustRep().getRepOffice().getEmployees().forEach(System.out::println);
+
+
+
+
         pdao.findEntityById("2A45C").getOrders().forEach(f -> System.out.println(f.getCust().getCustRep()));
         sdao.findEntityById(new BigDecimal(108)).getRepOffice().getEmployees().forEach(System.out::println);
         System.out.println(cdao.findEntityById(new BigDecimal(2111)).getCustRep().getManager().getManager().getManager());
